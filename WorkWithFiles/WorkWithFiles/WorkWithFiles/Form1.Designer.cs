69134312eeb7dc5bc58f18ef3592f09bd44733dd
marik14 <73874104+marik14@users.noreply.github.com>
2021-03-07 18:37:49 +0200

    Add files via upload

---
+++ WorkWithFiles/WorkWithFiles/WorkWithFiles/Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace WorkWithFiles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtOnerName = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtBusiName = new System.Windows.Forms.TextBox();
+            this.txtNumOfEmploy = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnShowFile = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtOld = new System.Windows.Forms.TextBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtDelete = new System.Windows.Forms.TextBox();
+            this.btnDeleteAll = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtNew = new System.Windows.Forms.TextBox();
+            this.lblEror = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtOnerName
+            // 
+            this.txtOnerName.Location = new System.Drawing.Point(571, 61);
+            this.txtOnerName.Name = "txtOnerName";
+            this.txtOnerName.Size = new System.Drawing.Size(103, 22);
+            this.txtOnerName.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(571, 183);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(208, 23);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "הוסף למאגר מס הכנסה";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtBusiName
+            // 
+            this.txtBusiName.Location = new System.Drawing.Point(571, 107);
+            this.txtBusiName.Name = "txtBusiName";
+            this.txtBusiName.Size = new System.Drawing.Size(103, 22);
+            this.txtBusiName.TabIndex = 2;
+            // 
+            // txtNumOfEmploy
+            // 
+            this.txtNumOfEmploy.Location = new System.Drawing.Point(571, 155);
+            this.txtNumOfEmploy.Name = "txtNumOfEmploy";
+            this.txtNumOfEmploy.Size = new System.Drawing.Size(103, 22);
+            this.txtNumOfEmploy.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label1.Location = new System.Drawing.Point(578, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(210, 32);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "עבודה עם קבצים";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(691, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "שם בעל העסק";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(718, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "שם העסק";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(697, 160);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "מספר עובדים";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(37, 61);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(437, 310);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "";
+            // 
+            // btnShowFile
+            // 
+            this.btnShowFile.Location = new System.Drawing.Point(266, 381);
+            this.btnShowFile.Name = "btnShowFile";
+            this.btnShowFile.Size = new System.Drawing.Size(208, 28);
+            this.btnShowFile.TabIndex = 9;
+            this.btnShowFile.Text = "הצג מאגר עסקים ";
+            this.btnShowFile.UseVisualStyleBackColor = true;
+            this.btnShowFile.Click += new System.EventHandler(this.btnShowFile_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(707, 226);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "עדכון מאגר";
+            // 
+            // txtOld
+            // 
+            this.txtOld.Location = new System.Drawing.Point(571, 246);
+            this.txtOld.Name = "txtOld";
+            this.txtOld.Size = new System.Drawing.Size(176, 22);
+            this.txtOld.TabIndex = 11;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(571, 302);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(208, 27);
+            this.btnUpdate.TabIndex = 12;
+            this.btnUpdate.Text = "עדכן";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(571, 382);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(208, 27);
+            this.btnDelete.TabIndex = 13;
+            this.btnDelete.Text = "מחק";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(699, 354);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(80, 17);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "מחיקת טקסט";
+            // 
+            // txtDelete
+            // 
+            this.txtDelete.Location = new System.Drawing.Point(571, 354);
+            this.txtDelete.Name = "txtDelete";
+            this.txtDelete.Size = new System.Drawing.Size(122, 22);
+            this.txtDelete.TabIndex = 15;
+            // 
+            // btnDeleteAll
+            // 
+            this.btnDeleteAll.Location = new System.Drawing.Point(37, 382);
+            this.btnDeleteAll.Name = "btnDeleteAll";
+            this.btnDeleteAll.Size = new System.Drawing.Size(208, 28);
+            this.btnDeleteAll.TabIndex = 16;
+            this.btnDeleteAll.Text = "מחק מאגר עסקים ";
+            this.btnDeleteAll.UseVisualStyleBackColor = true;
+            this.btnDeleteAll.Click += new System.EventHandler(this.btnDeleteAll_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(753, 246);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(26, 17);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "ישן";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(747, 274);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(32, 17);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "חדש";
+            // 
+            // txtNew
+            // 
+            this.txtNew.Location = new System.Drawing.Point(571, 274);
+            this.txtNew.Name = "txtNew";
+            this.txtNew.Size = new System.Drawing.Size(176, 22);
+            this.txtNew.TabIndex = 19;
+            // 
+            // lblEror
+            // 
+            this.lblEror.AutoSize = true;
+            this.lblEror.Location = new System.Drawing.Point(474, 424);
+            this.lblEror.Name = "lblEror";
+            this.lblEror.Size = new System.Drawing.Size(0, 17);
+            this.lblEror.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblEror);
+            this.Controls.Add(this.txtNew);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btnDeleteAll);
+            this.Controls.Add(this.txtDelete);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.txtOld);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnShowFile);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNumOfEmploy);
+            this.Controls.Add(this.txtBusiName);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtOnerName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtOnerName;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtBusiName;
+        private System.Windows.Forms.TextBox txtNumOfEmploy;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnShowFile;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtOld;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtDelete;
+        private System.Windows.Forms.Button btnDeleteAll;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtNew;
+        private System.Windows.Forms.Label lblEror;
+    }
+}
+
---
+++ WorkWithFiles/WorkWithFiles/WorkWithFiles/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WorkWithFiles
+{
+    public partial class Form1 : Form
+    {
+        string path = @"../../file.txt";
+        string nameOfManager;
+        string nameOfBussiness;
+        int newEmploee;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnShowFile_Click(object sender, EventArgs e)
+        {
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string text = sr.ReadToEnd();
+                richTextBox1.Text = text;
+            }
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            nameOfManager = txtOnerName.Text;
+            nameOfBussiness = txtBusiName.Text;
+            newEmploee = Convert.ToInt32(txtNumOfEmploy.Text);
+            string AllInfo = "Owner Name:" + nameOfManager + "\n Bussiness He Own:," +
+                nameOfBussiness + "\n Number Of Emploee Added:" + newEmploee + "\n";
+            Console.WriteLine("**************************");
+            File.AppendAllText(path, AllInfo);
+            MessageBox.Show("All Been Added");
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string find = txtOld.Text.Trim(); //txtFind is textbox and will have the text that we want to find and replace
+                string replace = txtNew.Text.Trim(); //txtReplace is text and it will replace the find text with Replace text
+                string newText = richTextBox1.Text.Replace(find, replace);
+                richTextBox1.Text = newText;
+
+                //Write the new contents of rich text box to file
+                File.WriteAllText(path, richTextBox1.Text.Trim());
+                MessageBox.Show(find + " Replaced with" + replace);
+
+            }
+            catch (Exception ex)
+            {
+                lblEror.Text = ex.Message;
+            }
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            string tempFile = Path.GetTempFileName();
+            using (var sr = new StreamReader(path))
+            using (var sw = new StreamWriter(tempFile))
+            {
+                string line;
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    if (!line.Contains(this.txtDelete.Text))
+                        sw.WriteLine(line);//remember the string in tempfile.
+                    else
+                    {
+                        sw.WriteLine(line.Replace(this.txtDelete.Text, ""));//replace the string with empty string.
+                        MessageBox.Show(this.txtDelete.Text + " Deleted");
+                    }
+
+                }
+            }
+
+            File.Delete(path);
+            File.Move(tempFile, path);//write back to the file with new data.
+        }
+
+        private void btnDeleteAll_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText(path, String.Empty);
+            MessageBox.Show("ALL Been Deleted");
+        }
+
+       
+    }
+}
